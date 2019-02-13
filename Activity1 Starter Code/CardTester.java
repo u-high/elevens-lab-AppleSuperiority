849832a95368114a9d11f6d0ff5c20c74c97b47d
AppleSuperiority <titanswrath288@gmail.com>
2019-02-13 14:55:20 -0600

    Day 1 (Feb 13, 2019)
    made the Card.java file and the CardTester.java file

--- Activity1 Starter Code/CardTester.java
+++ Activity1 Starter Code/CardTester.java
@@ -3,11 +3,18 @@
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+ /**
+  * The main method in this class checks the Card operations for consistency.
+  * @param args is not used.
+  */
+  
+ public static void main(String[] args) {
+  /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+   Card deckOne= new Card("stephen", "spade", 10);
+   System.out.println(deckOne);
+   System.out.println(deckOne.rank());
+   System.out.println(deckOne.suit());
+   System.out.println(deckOne.pointValue());
+ }
 }
+

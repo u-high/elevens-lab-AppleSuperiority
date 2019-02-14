2e574a38b104896064304f2cdac619ea67242651
AppleSuperiority <titanswrath288@gmail.com>
2019-02-14 15:00:05 -0600

    did the deck and decktester file yee

--- Activity2  Starter Code/DeckTester.java
+++ Activity2  Starter Code/DeckTester.java
@@ -3,11 +3,17 @@
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+ /**
+  * The main method in this class checks the Deck operations for consistency.
+  * @param args is not used.
+  */
+ public static void main(String[] args) {
+   String[] ranks = {"Ace","King","Queen","Jack","10","9","8","7","6","5","4","3","2"};
+   String[] suits = {"Clubs","Spades","Diamonds","Hearts"};
+   int[] values = {14,13,12,11,10,9,8,7,6,5,4,3,2};
+   Deck run = new Deck(ranks, suits, values);
+   System.out.println(run.size());
+   System.out.println(run.deal());
+ }
 }
+
